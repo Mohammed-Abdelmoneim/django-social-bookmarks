5293826729d9dcc624b8d70224d917286bd604a2
Mohammed Abdelmoneim <mohamedabdelmoneim95@gmail.com>
2025-07-17 21:23:07 +0300

    bookmarks app

---
+++ bookmarks/urls.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('account/', include('account.urls')),
+    path('social-auth/', include('social_django.urls', namespace='social')),
+    path('images/', include('images.urls', namespace='images')),
+]
+# The static() helper function serve media files with the Django development server
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
